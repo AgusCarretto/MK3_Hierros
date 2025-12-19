89d08e8d00020fc1d0fddc3444b9e08f3ffe8b6c
Agustin Carretto <agus.carretto@gmail.com>
2025-12-19 17:35:52 -0300

    FRONT PRONTO

--- mk3_hierros_back/src/main.ts
+++ mk3_hierros_back/src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // 1. Cambiar el create para usar NestExpressApplication
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  
- app.enableCors({ //Esto habilita el accesso desde el frontend 
-    origin: 'http://localhost:3001', // La URL del frontend
+  app.enableCors({
+    origin: 'http://localhost:3001',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
+  // 2. Configurar la carpeta estática
+  // Esto hace que si vas a http://localhost:3000/uploads/foto.jpg se pueda ver la imagen
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
